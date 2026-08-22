835e9a2667d04a8661334a453d950437c661d0ca
sowmia-r05 <sowmya.ramesh@kaisolutions.ai>
2026-08-22 05:58:16 +0530

    changes

--- EduTech_Full/naplan-backend/src/app.js
+++ EduTech_Full/naplan-backend/src/app.js
@@ -160,7 +160,7 @@ app.use(
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "Cache-Control", "Accept"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cache-Control", "Pragma", "Expires", "Accept"], 
   }),
 );
 
